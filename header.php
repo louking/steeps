7d416dbd83c7130d1619facdb076cd71f03c3f81
Lou King <lking@pobox.com>
2014-12-07 13:05:17 -0500

    partial update to banner to prettify
    * partial fix for #41

--- header.php
+++ header.php
@@ -34,47 +34,54 @@
 
 <div id="background" class="background">
 <div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'steeps' ); ?></a>
+    <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'steeps' ); ?></a>
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div>
+    <header id="masthead" class="site-header" role="banner">
+        <div class="site-branding">
+            <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+            <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+        </div>
 
-		<table border="0" cellspacing="0" cellpadding="0">
-		  <tr class="banner-image">
-			<th colspan="1" scope="col" style="border:0px; padding: 2px"><img src="<?php echo get_site_url(); ?>/wp-content/uploads/2014/12/fsrc-logo-2014.jpg" alt="Banner" height="120"/></th>
-			<th colspan="1" scope="col"><img src="http://www.steeplechasers.org/images/HomePage1/Steeps1.jpg" alt="Click to change" name="titleImage" height="120" id="titleImage" style="cursor:hand; border:0" onclick="choosePic(1)"/></th>
-			<th colspan="1" scope="col"><img src="http://www.steeplechasers.org/images/HomePage1/Steeps2.jpg" alt="Click to change" name="titleImage2" height="120" id="titleImage2" style="cursor:hand; border:0" onclick="choosePic(2)"/></th>
-			<th colspan="1" scope="col"><img src="http://www.steeplechasers.org/images/HomePage1/Steeps3.jpg" alt="Click to change" name="titleImage3" height="120" id="titleImage3" style="cursor:hand; border:0" onclick="choosePic(3)"/></th>
-		  </tr>
-		</table>
+        <?php /*
+        <table border="0" cellspacing="0" cellpadding="0">
+          <tr class="banner-image">
+            <th colspan="1" scope="col" style="border:0px; padding: 2px"><img src="<?php echo get_site_url(); ?>/wp-content/uploads/2014/12/fsrc-logo-2014.jpg" alt="Banner" height="120"/></th>
+            <th colspan="1" scope="col"><img src="http://www.steeplechasers.org/images/HomePage1/Steeps1.jpg" alt="Click to change" name="titleImage" height="120" id="titleImage" style="cursor:hand; border:0" onclick="choosePic(1)"/></th>
+            <th colspan="1" scope="col"><img src="http://www.steeplechasers.org/images/HomePage1/Steeps2.jpg" alt="Click to change" name="titleImage2" height="120" id="titleImage2" style="cursor:hand; border:0" onclick="choosePic(2)"/></th>
+            <th colspan="1" scope="col"><img src="http://www.steeplechasers.org/images/HomePage1/Steeps3.jpg" alt="Click to change" name="titleImage3" height="120" id="titleImage3" style="cursor:hand; border:0" onclick="choosePic(3)"/></th>
+          </tr>
+        </table>
+        */?>
+        
+        <div class="header-images">
+            <img class="banner-logo" src="<?php echo get_site_url(); ?>/wp-content/uploads/2014/12/fsrc-logo-2014.jpg" alt="Banner"/>
+            <img class="banner-image" src="http://www.steeplechasers.org/images/HomePage1/Steeps1.jpg" alt="Click to change" name="titleImage" id="titleImage" style="cursor:hand; border:0" onclick="choosePic(1)"/>
+        </div>
 
-		<div class="navigation-area">
-			<div class="navigation-button-area">
-				<div class="navigation-buttons">
-						<a href="https://www.facebook.com/groups/fredericksteeplechasers/" target="_blank"><img src="<?php echo get_site_url(); ?>/wp-content/uploads/2014/11/FB_FindUsOnFacebook-114-e1417379795693.jpg"></a>
-						<?php echo do_shortcode('[maxbutton name="join"]'); ?>
-				</div>
-				<div class="breadcrumbs-buttons">
-					<?php echo do_shortcode('[maxbutton name="race results"]'); ?>
-				</div>
-			</div><!-- .navigation-buttons -->
+        <div class="navigation-area">
+            <div class="navigation-button-area">
+                <div class="navigation-buttons">
+                        <a href="https://www.facebook.com/groups/fredericksteeplechasers/" target="_blank"><img src="<?php echo get_site_url(); ?>/wp-content/uploads/2014/11/FB_FindUsOnFacebook-114-e1417379795693.jpg"></a>
+                        <?php echo do_shortcode('[maxbutton name="join"]'); ?>
+                </div>
+                <div class="breadcrumbs-buttons">
+                    <?php echo do_shortcode('[maxbutton name="race results"]'); ?>
+                </div>
+            </div><!-- .navigation-buttons -->
 
-			<nav id="site-navigation" class="main-navigation" role="navigation">
-				<button class="menu-toggle"><?php _e( 'Primary Menu', 'steeps' ); ?></button>
-				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-			</nav><!-- #site-navigation -->
-			
-			<div class="breadcrumbs">
-				<?php if(function_exists('bcn_display'))
-				{
-				    bcn_display();
-				}?>
-			</div><!-- #breadcrumbs -->
+            <nav id="site-navigation" class="main-navigation" role="navigation">
+                <button class="menu-toggle"><?php _e( 'Primary Menu', 'steeps' ); ?></button>
+                <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+            </nav><!-- #site-navigation -->
+            
+            <div class="breadcrumbs">
+                <?php if(function_exists('bcn_display'))
+                {
+                    bcn_display();
+                }?>
+            </div><!-- #breadcrumbs -->
 
-		</div><!-- #navigation-area -->
-	</header><!-- #masthead -->
+        </div><!-- #navigation-area -->
+    </header><!-- #masthead -->
 
-	<div id="content" class="site-content">
+    <div id="content" class="site-content">
