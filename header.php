478dd855a0fb0d83b250420f6aede890c031a417
Lou King <lking@pobox.com>
2014-12-17 15:40:30 -0500

    better support  for touch screen submenus
    * fixes #89
    * from https://github.com/Automattic/_s/pull/666 and
    https://github.com/Automattic/_s/commit/d8d89bc6818849849f58da3ff890d45044735dc5

--- header.php
+++ header.php
@@ -57,7 +57,7 @@
 
             <div class="navigation-area">
                 <nav id="site-navigation" class="main-navigation" role="navigation">
-                    <button class="menu-toggle"><?php _e( 'Menu', 'steeps' ); ?></button>
+                    <button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Menu', 'steeps' ); ?></button>
                     <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
                 </nav><!-- #site-navigation -->
                 
