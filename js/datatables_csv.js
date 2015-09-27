77c4b4163b2f87611936cf15c8a8b88f5a9b2253
Lou King <lking@pobox.com>
2015-09-27 16:40:07 -0400

    add membership summary
    * keep track of registered members
    * final fix #155

---
+++ js/datatables_csv.js
@@ -0,0 +1,34 @@
+// assumes empty table in DOM before call
+function datatables_csv(fileuri, tableid, dtoptions) {
+  if (typeof dtoptions === 'undefined') { dtoptions = {}; }
+
+  d3.text(fileuri, function (datasetText) {
+
+    var rows = d3.csv.parseRows(datasetText);
+
+    var tbl = d3.select("#" + tableid);
+
+    // headers
+    tbl.append("thead").append("tr")
+      .selectAll("th")
+        .data(rows[0])
+      .enter().append("th")
+        .text(function(d) { return d; });
+
+    // data
+    tbl.append("tbody")
+      .selectAll("tr")
+          .data(rows.slice(1))
+        .enter().append("tr")
+
+      .selectAll("td")
+          .data(function(d){ return d; })
+        .enter().append("td")
+          .text(function(d){ return d; });
+
+    jQuery(document).ready(function() {
+      jQuery("#" + tableid).DataTable(dtoptions);
+    });
+
+  });
+}
---
+++ js/membership-summary.js
@@ -0,0 +1,6 @@
+var dtoptions = {
+    "order": [[1,'asc']],
+    dom: '<"clear">lfrtip',
+}
+
+datatables_csv("/wp-content/uploads/membership/membership-summary.csv", "membership-table", dtoptions);
